c9e10d7f5cc6aa8505dc9f1514a11ca436a6f0e2
Maksym Malyi <malyimaksym37@gmail.com>
2026-02-06 09:43:58 +0200

    looks like done but have a bug in geting current day of the week

--- src/Advisor.java
+++ src/Advisor.java
@@ -10,7 +10,7 @@ class AdvisorImpl implements AdvisorInterface {
             case WEDNESDAY -> "WEDNESDAY - it is middle of working week... you still have to be focused but it is easier the previous days";
             case THURSDAY -> "THURSDAY - almost on finish, one more day and that's all";
             case FRIDAY -> "FRIDAY - almost the weekend.... you can start to relax";
-            case SATURDAY -> "SATURDAY - atlast ... it is the weekend ... now you can relax ...  but only after you will do you home tasks";
+            case SATURDAY -> "SATURDAY - atlast ... it is the weekend ... now you can relax ...  but only after you will do your home tasks";
             case SUNDAY -> "SUNDAY - it is your day, do whatever you want to";
         };
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 void main() {
     AdvisorImpl advisor = new AdvisorImpl();
+    Calendar c = Calendar.getInstance();
+    int dayOfWeek = c.get(Calendar.DAY_OF_WEEK) - 1;
+    Day today = Day.values()[dayOfWeek];
+    IO.println(advisor.advise(today));
+    /*
     for (Day day : Day.values()) {
         IO.println(advisor.advise(day));
     }
+    */
 }
